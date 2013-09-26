faf81a8f0729a49e60a16d6f2c0679ff2fa2f8e2
Daniel Cannon <daniel@danielcannon.co.uk>
2013-09-26 14:26:23 +0100

    Added the comment model

--- models/post.go
+++ models/post.go
@@ -49,3 +49,15 @@ func (p *Post) Rank() float64 {
 
 	return (order + float64((sign*seconds)/45000))
 }
+
+type Comment struct {
+	Id         string `gorethink:"id,omitempty"`
+	Post       string
+	AuthorId   string
+	AuthorName string
+	Content    string `schema:"content"`
+	Likes      int
+	Dislikes   int
+	Created    time.Time
+	Modified   time.Time
+}
